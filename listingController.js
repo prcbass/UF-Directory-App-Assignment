b8ba7cd892bd12bc691ea5e7527d5ede9d7e641e
Sebastian Hernandez <cbasshernandez@gmail.com>
2016-01-12 12:39:16 -0500

    Started implemented controller functions.

--- listingController.js
+++ listingController.js
@@ -1,5 +1,6 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
+    $scope.newDItem = '';
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
@@ -7,8 +8,17 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push($scope.newDItem);
+      $scope.newDItem = '';
+    };
+
+    $scope.deleteListing = function(index) {
+      $scope.listing.splice(index,1);
+    };
+
+    $scope.showDetails = function(index) {
+      //Needs to be implemented
+    };
   }
 ]);
